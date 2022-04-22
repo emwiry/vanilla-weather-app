4f3f6a8780a4315a6134d1923dc2767dd9378090
emwiry <emangel731@gmail.com>
2022-04-22 15:31:34 -0600

    Integrating axios into HTML & Creating JS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
 
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "b7e0ef1260e02738f447532858962b61";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Portland&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
